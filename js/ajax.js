1438b3b407f4d9647aa682f2d74d7dcac855c8c4
unknown <you@example.com>
2020-11-14 10:06:44 +0100

    $.getJSON method

--- js/ajax.js
+++ js/ajax.js
@@ -29,16 +29,32 @@ function generateJoke() {
 
                 //for the IT API link
                 //$('#showMe').html(data.value.joke); 
-                
+
                 //https secure link
                 $('#showMe').html(data.value);//
             },
             //not too good, eh honey
             error: function (req, status, error) {
                 //show me
-                alert(`${req} ${status} ${error}`);
+                console.log(`${req} ${status} ${error}`);
                 
             }
         }
     );
+    //IT way
+    $.getJSON( "https://api.chucknorris.io/jokes/random", "json", function(data){
+        //iterate through it
+        $('#showMe2').text(data.value);
+        //how to place the error here?
+    })
+    .done(function() {
+        console.log( "second success" );
+      })
+      .fail(function() {
+        console.log( "error" );
+      })
+      .always(function() {
+        console.log( "complete" );
+      });
+
 }
